75f41b9d9de4c93f06cee8f85651138104007686
Connectmcc <connectmcc.dev@gmail.com>
2024-01-02 16:49:02 +0530

    collapsed

--- src/Components/Posts/PostCard.jsx
+++ src/Components/Posts/PostCard.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const PostCard = () => {
   return (
     <div>
-        <div>
-            <div>
-                <div  className=''>
+        <div className='border rounded-md w-full'>
+            <div className='flex justify-between items-center w-full py-4 px-5'>
+                <div  className='flex items-center'>
                     <img  className='h-12 w-12 rounded-full'
                      src ="https://cdn.pixabay.com/photo/2023/07/29/06/28/bird-8156308_1280.jpg "
                     alt=" "
@@ -14,6 +14,9 @@ const PostCard = () => {
                         <p className="font-semibold text-sm ">username</p>
                         <p className="font-thin text-sm"> location</p>
                     </div>
+                </div>
+                <div>
+               
                 </div>
             </div>
         </div>
