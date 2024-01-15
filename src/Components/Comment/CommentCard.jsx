1a3ff47f08636a8bd6d3819c0a8fc8579e8632a9
unknown <connectmcc.dev@gmail.com>
2024-01-15 17:19:29 +0530

    commit

--- src/Components/Comment/CommentCard.jsx
+++ src/Components/Comment/CommentCard.jsx
@@ -1,23 +1,31 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import {AiFillHeart,AiOutlineHeart} from "react-icons/ai"
 const CommentCard = () => {
+    const [isCommentLike,setIsCommentlike]=useState();
   return (
     <div>
 <div>
-    <div>
-        <div className='flex items-center'>
+    <div className='flex items-center'>
+        <div >
             <img className='w-9 h-9 rounded-full' src="https://cdn.pixabay.com/photo/2023/12/05/01/15/desert-8430551_1280.jpg" alt="" />
         </div>
         <div className='ml-3'>
             <p><span className='font-semibold'>username</span>
             <span className='ml-2'>lovely! post man!</span>
             </p>
-            <div className='flex items-center space-x-3 '>
+            <div className='flex items-center space-x-3 text-xs opacity-60 pt-2 '>
                 <span>2 min ago</span>
                 <span> 23 likes</span>
             </div>
+
         </div>
     </div>
+
+{isCommentLike ? (
+<AiFillHeart className=" text-xs hover:opacity-50 cursor-pointer  text-red-600"/>
+) : (
+<AiOutlineHeart  className=" text-xs hover:opacity-50 cursor-pointer"/>
+)}
 </div>
 
     </div>
