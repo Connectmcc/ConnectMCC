4c3958f4e5e171cc2742d1a1908498947b6a3313
unknown <connectmcc.dev@gmail.com>
2024-01-16 10:28:24 +0530

    commit

--- src/Components/Comment/CommentCard.jsx
+++ src/Components/Comment/CommentCard.jsx
@@ -1,19 +1,28 @@
 import React, { useState } from 'react'
-import {AiFillHeart,AiOutlineHeart} from "react-icons/ai"
+import {AiFillHeart,AiOutlineHeart} from "react-icons/ai" ;
 const CommentCard = () => {
     const [isCommentLike,setIsCommentlike]=useState();
+
+    const handleLikedComment=()=>{
+        setIsCommentlike(isCommentLike)
+    }
   return (
     <div>
-<div>
+      <div  className="flex items-center justify-between py-5">
     <div className='flex items-center'>
         <div >
-            <img className='w-9 h-9 rounded-full' src="https://cdn.pixabay.com/photo/2023/12/05/01/15/desert-8430551_1280.jpg" alt="" />
+            <img
+             className="w-9 h-9 rounded-full"
+              src="https://cdn.pixabay.com/photo/2023/12/05/01/15/desert-8430551_1280.jpg"
+               alt=""
+                />
         </div>
         <div className='ml-3'>
-            <p><span className='font-semibold'>username</span>
+            <p>
+                <span className='font-semibold'>username</span>
             <span className='ml-2'>lovely! post man!</span>
             </p>
-            <div className='flex items-center space-x-3 text-xs opacity-60 pt-2 '>
+            <div className="flex items-center space-x-3 text-xs opacity-60 pt-2 ">
                 <span>2 min ago</span>
                 <span> 23 likes</span>
             </div>
@@ -22,9 +31,9 @@ const CommentCard = () => {
     </div>
 
 {isCommentLike ? (
-<AiFillHeart className=" text-xs hover:opacity-50 cursor-pointer  text-red-600"/>
+<AiFillHeart onClick={handleLikedComment}className=" text-xs hover:opacity-50 cursor-pointer text-red-600 "/>
 ) : (
-<AiOutlineHeart  className=" text-xs hover:opacity-50 cursor-pointer"/>
+<AiOutlineHeart nClick={handleLikedComment} className=" text-xs hover:opacity-50 cursor-pointer"/>
 )}
 </div>
 
